7c88bd26e1799f1769c9461ab8ad83c1d177dc94
rajnikant.nayak <rajnikantnayak100@gmail.com>
2024-11-05 11:20:35 +0530

    first commit

---
+++ src/main/java/com/example/openshifttestdeploy/OpenshiftTestDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.openshifttestdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshiftTestDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenshiftTestDeployApplication.class, args);
+	}
+
+}
